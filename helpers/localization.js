584f43ca5dd3ef221630a2b8d030a05ab56417e8
Kieran Anderson <Kieran.Anderson@D2L.com>
2020-09-10 14:50:51 -0400

    Add oslo call for language overrides
    - Call into the localize override call in core
    - Falls back to the current language terms if not available

--- helpers/localization.js
+++ helpers/localization.js
@@ -1,4 +1,6 @@
 import { resolveUrl } from '@polymer/polymer/lib/utils/resolve-url.js';
+import { getLocalizeOverrideResources } from '@brightspace-ui/core/helpers/getLocalizeResources.js';
+
 
 const SUPPORTED_LANGUAGES = ['ar-sa', 'ar', 'da-dk', 'da', 'de-de', 'de', 'en-ca', 'en-gb', 'en-us', 'en',
 	'es-mx', 'es', 'fi-fl', 'fi', 'fr-ca', 'fr-fr', 'fr', 'ja-jp', 'ja', 'ko-kr', 'ko', 'nb-no', 'nb',
@@ -10,6 +12,11 @@ export async function getLocalizeResources(langs, baseUrl) {
 	const supportedLanguages = langs.reverse().filter(language => {
 		return SUPPORTED_LANGUAGES.indexOf(language) > -1;
 	});
+
+	function resolveOverridesFunc() {
+		//package+component
+		return '@d2l\\d2l-attachment\\attachment';
+	}
 
 	const sergeLangterms = supportedLanguages.map(language => {
 		const url = resolveUrl(`../locales/${language}.json`, baseUrl);
@@ -35,9 +42,10 @@ export async function getLocalizeResources(langs, baseUrl) {
 	responses.forEach(language => {
 		Object.assign(langterms, language);
 	});
-
-	return {
-		language: supportedLanguages[supportedLanguages.length - 1],
-		resources: langterms
-	};
+	
+	return await getLocalizeOverrideResources(
+		supportedLanguages[supportedLanguages.length - 1],
+		langterms,
+		resolveOverridesFunc
+	);
 }
